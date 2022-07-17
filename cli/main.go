c0e2dd5ebbf9a3633d6800be1ae0f48b473abf24
korneevsergey <seriozhakorneev@gmail.com>
2022-07-17 23:51:14 +0500

    refactor
    More refactor.
    Experiments with Stringer.

--- cli/main.go
+++ cli/main.go
@@ -8,7 +8,33 @@ import (
 	"unicode"
 )
 
-func stringToBraille(s string) (top string, middle string, bottom string) {
+type braillePrint struct {
+	top, middle, bottom string
+}
+
+func (b braillePrint) String() string {
+	//len of row with 10 Braille symbols(130)
+	lengthOfRow := 130
+	firstIndex := 0
+	for i := 1; i <= len(b.top)/lengthOfRow; i++ {
+		fmt.Println(strings.Trim(b.top[firstIndex:lengthOfRow], "[]"))
+		fmt.Println(strings.Trim(b.middle[firstIndex:lengthOfRow], "[]"))
+		fmt.Println(strings.Trim(b.bottom[firstIndex:lengthOfRow], "[]"))
+		fmt.Printf("\n")
+		firstIndex += 130
+		lengthOfRow += 130
+	}
+
+	if len(b.top)%130 != 0 {
+		fmt.Println(strings.Trim(b.top[firstIndex:], "[]"))
+		fmt.Println(strings.Trim(b.middle[firstIndex:], "[]"))
+		fmt.Println(strings.Trim(b.bottom[firstIndex:], "[]"))
+		fmt.Printf("\n")
+	}
+	return ""
+}
+
+func stringToBraillePrint(s string) (top string, middle string, bottom string) {
 	for _, letter := range s {
 		// add capitalized letter symbol
 		if strings.ToUpper(string(letter)) == string(letter) && unicode.IsLetter(letter) {
@@ -17,11 +43,9 @@ func stringToBraille(s string) (top string, middle string, bottom string) {
 			middle += capSlice[2] + capSlice[3] + " "
 			bottom += capSlice[4] + capSlice[5] + " "
 		}
-
 		// making letter lower
 		lowerLetter := strings.ToLower(string(letter))
 		slice := strings.SplitN(braille[lowerLetter], " ", -1)
-
 		// make lower letter
 		top += slice[0] + slice[1] + " "
 		middle += slice[2] + slice[3] + " "
@@ -30,33 +54,19 @@ func stringToBraille(s string) (top string, middle string, bottom string) {
 	return top, middle, bottom
 }
 
-// TODO: Refactor this, it can be easier
-func printBraille(s string) {
-	top, middle, bottom := stringToBraille(s)
-
-	//len of row with 10 Braille symbols(130)
-	lengthOfRow := 130
-	firstIndex := 0
-	for i := 1; i <= len(top)/lengthOfRow; i++ {
-
-		fmt.Println(strings.Trim(top[firstIndex:lengthOfRow], "[]"))
-		fmt.Println(strings.Trim(middle[firstIndex:lengthOfRow], "[]"))
-		fmt.Println(strings.Trim(bottom[firstIndex:lengthOfRow], "[]"))
-		fmt.Printf("\n")
-
-		firstIndex += 130
-		lengthOfRow += 130
+func textValidator(str string) bool {
+	for _, charVariable := range str {
+		if (charVariable < 'a' || charVariable > 'z') &&
+			(charVariable < 'A' || charVariable > 'Z') &&
+			(charVariable != ' ') {
+			return false
+		}
 	}
-
-	if len(top)%130 != 0 {
-		fmt.Println(strings.Trim(top[firstIndex:], "[]"))
-		fmt.Println(strings.Trim(middle[firstIndex:], "[]"))
-		fmt.Println(strings.Trim(bottom[firstIndex:], "[]"))
-		fmt.Printf("\n")
-	}
+	return true
 }
 
-func cliFlow() {
+func main() {
+	fmt.Println("---------English to Braille---------")
 	for {
 		fmt.Print("Enter text (A-Z a-z . , ex1t): ")
 
@@ -67,11 +77,14 @@ func cliFlow() {
 			break
 		}
 		fmt.Print("\n")
-		printBraille(scanner.Text())
+		b := braillePrint{}
+		s := scanner.Text()
+		if textValidator(s) {
+			// TODO:
+			// эти символы принтуются в консоль как хотят особенно если строка больше 42
+			// где-то разъехалось, надо чинить
+			b.top, b.middle, b.bottom = stringToBraillePrint(s)
+			fmt.Println(b)
+		}
 	}
-}
-
-func main() {
-	fmt.Println("---------English to Braille---------")
-	cliFlow()
 }
